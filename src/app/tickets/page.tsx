e0e7edd61c5e00203c21b768a9ad67c5d716ac09
Amorem <dev@amorem.com>
2024-12-06 16:07:18 +0100

    refactoring

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,21 +1,6 @@
-import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
 import Heading from "@/components/heading";
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "../paths";
-
-const TICKET_ICONS = {
-	OPEN: <LucideFileText />,
-	IN_PROGRESS: <LucidePencil />,
-	DONE: <LucideCircleCheck />,
-};
+import TicketItem from "@/features/ticket/component/ticket-item";
 
 export default function TicketsPage() {
 	return (
@@ -24,29 +9,7 @@ export default function TicketsPage() {
 			<div className="flex flex-1 flex-col items-center gap-y-4 animate-fade-in-from-top">
 				{initialTickets.map((ticket) => {
 					//console.log("@@@ DEBUG", ticketPath(ticket.id));
-					return (
-						<Card key={ticket.id} className="w-full max-w-[420px]">
-							<CardHeader>
-								<CardTitle className="flex gap-x-2">
-									<span>{TICKET_ICONS[ticket.status]}</span>
-									<span className=" truncate">{ticket.title}</span>
-								</CardTitle>
-							</CardHeader>
-							<CardContent>
-								<span className="line-clamp-3 whitespace-break-spaces">
-									{ticket.content}
-								</span>
-							</CardContent>
-							<CardFooter>
-								<Link
-									href={ticketPath(ticket.id)}
-									className="text-sm underline"
-								>
-									view
-								</Link>
-							</CardFooter>
-						</Card>
-					);
+					return <TicketItem key={ticket.id} ticket={ticket} />;
 				})}
 			</div>
 		</div>
