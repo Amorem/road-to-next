f7fa09d5f0673416281c42118749fa2d2de2fcec
Amorem <dev@amorem.com>
2024-12-07 22:29:19 +0100

    fix missing tailwind path for features folder

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
 import Heading from "@/components/heading";
+import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/component/ticket-list";
 
 export default function TicketsPage() {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <Suspense fallback={<div>Loading</div>}>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
--- src/features/ticket/component/ticket-item.tsx
+++ src/features/ticket/component/ticket-item.tsx
@@ -8,46 +8,46 @@ import { TICKET_ICONS } from "@/features/ticket/constants";
 import { Ticket } from "@/features/ticket/types";
 
 type TicketItemProps = {
-	ticket: Ticket;
-	isDetail?: boolean;
+  ticket: Ticket;
+  isDetail?: boolean;
 };
 
 export default function TicketItem({ ticket, isDetail }: TicketItemProps) {
-	const detailButton = (
-		<Button variant={"outline"} asChild size={"icon"}>
-			<Link href={ticketPath(ticket.id)}>
-				<LucideSquareArrowUpRight className="h-4 w-4" />
-			</Link>
-		</Button>
-	);
+  const detailButton = (
+    <Button variant={"outline"} asChild size={"icon"}>
+      <Link href={ticketPath(ticket.id)}>
+        <LucideSquareArrowUpRight className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
 
-	return (
-		<div
-			className={clsx("w-full flex gap-x-1", {
-				"max-w-[580px]": isDetail,
-				"max-w-[420px]": !isDetail,
-			})}
-		>
-			<Card className="w-full ">
-				<CardHeader>
-					<CardTitle className="flex gap-x-2">
-						<span>{TICKET_ICONS[ticket.status]}</span>
-						<span className=" truncate">{ticket.title}</span>
-					</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<span
-						className={clsx(" whitespace-break-spaces", {
-							"line-clamp-3": !isDetail,
-						})}
-					>
-						{ticket.content}
-					</span>
-				</CardContent>
-			</Card>
-			{isDetail ? null : (
-				<div className="flex flex-col gap-y-1">{detailButton}</div>
-			)}
-		</div>
-	);
+  return (
+    <div
+      className={clsx("w-full flex gap-x-1", {
+        "max-w-[580px]": isDetail,
+        "max-w-[420px]": !isDetail,
+      })}
+    >
+      <Card className="w-full ">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className=" truncate">{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <span
+            className={clsx(" whitespace-break-spaces", {
+              "line-clamp-3": !isDetail,
+            })}
+          >
+            {ticket.content}
+          </span>
+        </CardContent>
+      </Card>
+      {!isDetail ? null : (
+        <div className="flex flex-col gap-y-1">{detailButton}</div>
+      )}
+    </div>
+  );
 }
