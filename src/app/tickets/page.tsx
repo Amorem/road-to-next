b9e929193ba44c66dc4f4cfc796ea8389a527cb9
Amorem <dev@amorem.com>
2024-12-08 22:03:16 +0100

    create ticket

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,31 @@
 import { Suspense } from "react";
 import Heading from "@/components/heading";
 import { Spinner } from "@/components/spinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TicketCreateForm } from "@/features/ticket/component/ticket-create-form";
 import { TicketList } from "@/features/ticket/component/ticket-list";
 
 export default function TicketsPage() {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>Create Ticket</CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TicketCreateForm />
+        </CardContent>
+      </Card>
+
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
